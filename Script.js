2ce3c5e4f740c95016149c6b7431c38b7ad5b8bb
deerty666 <alraiee318@gmail.com>
2026-04-11 18:44:57 +0300

    Implement service worker registration
    Add service worker registration for PWA support.

--- Script.js
+++ Script.js
@@ -134,3 +134,12 @@ function clearCart() { cart = []; updateCart(); }
 
 render();
 updateCart();
+
+// كود تفعيل التثبيت كتطبيق
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('sw.js')
+            .then(reg => console.log('Service Worker Registered'))
+            .catch(err => console.log('Service Worker Error', err));
+    });
+}
