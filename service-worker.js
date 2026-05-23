78028dc37a5b261a72039b3e3d9fe4c55f1283d9
deerty666 <alraiee318@gmail.com>
2026-05-23 15:06:49 +0300

    Update cache name from v8 to v10

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "deerty-drivers-v8";
+const CACHE_NAME = "deerty-drivers-v10";
 const FILES = ["./","./index.html","./style.css","./app.js","./manifest.json","./icon-192.png","./icon-512.png"];
 
 self.addEventListener("install", e => {
